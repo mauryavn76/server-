afe8f3c22c65c2f740defb945b8c1f42d1851d36
mauryavn76 <51995096+mauryavn76@users.noreply.github.com>
2019-07-24 14:16:15 +0530

    Update testnode.test.js

--- testnode.test.js
+++ testnode.test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const app = require('./server/routes/index')
 
 describe('Test the root path of node', () =>{
-    testing('It should response the get method', () => {
+    test('It should response the get method', () => {
         request(app).get('/student').then((response) => {
             expect(response.statusCode).toBe(200);
            
